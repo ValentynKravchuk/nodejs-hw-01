0b200c58f5f27e4a19f2d83cc9023cdde03dbba3
ValentynKravchuk <167019009+ValentynKravchuk@users.noreply.github.com>
2025-01-11 22:58:09 +0100

    step 2

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const data = JSON.stringify(updatedContacts);
+  try {
+    await fs.writeFile(PATH_DB, data, 'utf8');
+  } catch (error) {
+    console.log('Помилка запису у файл:', error);
+  }
+};
