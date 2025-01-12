1218c76e1f0a22a31058075f73f7796772d68292
ValentynKravchuk <167019009+ValentynKravchuk@users.noreply.github.com>
2025-01-12 13:31:17 +0100

    step 8

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,14 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const allContacts = await readContacts();
+    const updatedContacts = allContacts.slice(0, allContacts.length - 1);
+    await writeContacts(updatedContacts);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 removeLastContact();
